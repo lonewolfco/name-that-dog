c97e97a6c4202d9d0a277ba24f30e2a087486688
Lo Selby <laurenxselby@gmail.com>
2021-11-28 14:53:52 -0800

    add start game function timer countdown initiated upon click of start game button added dog images and began var for questions

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+var startButton = document.querySelector(".start-button");
+var timerCount = document.querySelector(".timer-count");
+
+
+var timer;
+var timeLeft;
+
+var questions = [
+    {
+        question: "./assets/images/pug.jpeg",
+        multipleChoiceOptions: [
+            "Shih Tzu",
+            "Puggle",
+            "Yorkie",
+            "Pug"
+        ],
+        correct: "Pug"
+
+    }
+];
+
+startButton.addEventListener("click", startGame);
+
+// startGame function beings when the start game button is clicked & triggers other functions
+function startGame() {
+    alert("Get Ready...")
+    timeLeft = 60;
+    countDown ();
+    
+}
+
+// timer function 
+function countDown() {
+
+    timer = setInterval(function() {
+
+        if (timeLeft >= 1) {
+            timerCount.textContent = ": " + timeLeft + " Seconds";
+            timeLeft --;
+            // test if meets win condition & timerCount 
+        } if (timerCount === 0) {
+            clearInterval(timer);
+            // trigger loseGame();
+        }
+    }, 1000);
+}
+
+
